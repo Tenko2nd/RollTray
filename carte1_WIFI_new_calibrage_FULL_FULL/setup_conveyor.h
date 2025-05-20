5869b947ed8426057ee6863bd795c05ee20a955f
Mae R. <maeva.ramamonjyramboa@reseau.eseo.fr>
2025-05-20 22:46:10 +0200

    Add files via upload

---
+++ carte1_WIFI_new_calibrage_FULL_FULL/setup_conveyor.h
@@ -0,0 +1,44 @@
+#include <Arduino.h>
+#include "conveyor.h"
+#include "iot.h"
+
+extern bool calibrageFait;
+extern bool calibrageEnCours;
+extern bool bascule_etat_0;
+
+extern bool carte1_verifiee;
+extern bool carte2_verifiee;
+extern bool carte3_verifiee;
+extern bool carte4_verifiee;
+
+extern unsigned long t0;
+extern unsigned long t1;
+extern unsigned long t2;
+extern unsigned long t3;
+extern unsigned long t4;
+
+extern unsigned long t01;
+extern unsigned long t12;
+extern unsigned long t23;
+extern unsigned long t34;
+
+extern unsigned long tempsDepart;
+
+
+
+extern bool attenteNouvelleCarte0;
+
+extern const unsigned long TEMPS_MAX_CALIBRAGE;
+extern unsigned long temps_debut_calibrage;
+
+void reinitialize_variables();
+
+void setup_conveyor();
+
+bool toutesCartesVerifiees();
+
+// Check Timers
+bool checkTimerCard1(int key);
+bool checkTimerCard2(int key);
+bool checkTimerCard3(int key);
+bool checkTimerCard4(int key);
